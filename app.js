30c23705654acbbec651a62722e09591b311d1c7
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-03 12:42:54 -0500

    implement special attack

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ new Vue({
                 pAttack: `Player hits monster with ${playerAttack} damage`,
                 mAttack: `Monster hits player with ${monsterAttack} damage`
             });
+        },
+        specialAttack: function() {
+            let playerAttack = Math.floor(Math.random() * 10) + 10;
+            let monsterAttack = Math.floor(Math.random() * 10);
+
+            this.playerHealth -= monsterAttack;
+            this.monsterHealth -= playerAttack;
+
+            this.attackLog.unshift({
+                pAttack: `Player hits monster with a special attack causing ${playerAttack} damage`,
+                mAttack: `Monster hits player with ${monsterAttack} damage`
+            });
         }
     }
 });
