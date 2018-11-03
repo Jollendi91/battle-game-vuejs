84cd866c4b6b14801b432df9baed6fb4d0f6c2cc
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-03 13:13:29 -0500

    implement heal and giveup methods

--- app.js
+++ app.js
@@ -50,14 +50,32 @@ new Vue({
             });
         },
         specialAttack: function() {
-            let playerAttack = Math.floor(Math.random() * 10) + 10;
-            let monsterAttack = Math.floor(Math.random() * 10);
+            let playerAttack = Math.floor(Math.random() * 20);
+            let monsterAttack = Math.floor(Math.random() * 20);
 
             this.playerHealth -= monsterAttack;
             this.monsterHealth -= playerAttack;
 
             this.attackLog.unshift({
                 pAttack: `Player hits monster with a special attack causing ${playerAttack} damage`,
+                mAttack: `Monster hits player with ${monsterAttack} damage`
+            });
+        },
+        heal: function() {
+            let monsterAttack = Math.floor(Math.random() * 10);
+            let message;
+            this.playerHealth -= monsterAttack;
+            if (this.playerHealth + 10 > 100) {
+                message = `Player healed themself by ${100 - this.playerHealth}`;
+                this.playerHealth = 100;
+            }
+            else {
+                this.playerHealth += 10;
+                message = `Player healed themself by 10`;
+            }
+
+            this.attackLog.unshift({
+                pAttack: message,
                 mAttack: `Monster hits player with ${monsterAttack} damage`
             });
         }
