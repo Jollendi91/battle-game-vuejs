1efa6e75e7146e109b899624a93b60ec3de128cc
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-03 12:41:20 -0500

    implement attack method

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         attackLog: []
+    },
+    methods: {
+        attack: function() {
+            let playerAttack = Math.floor(Math.random() * 10);
+            let monsterAttack = Math.floor(Math.random() * 10);
+
+            this.playerHealth -= monsterAttack;
+            this.monsterHealth -= playerAttack;
+
+            this.attackLog.unshift({
+                pAttack: `Player hits monster with ${playerAttack} damage`,
+                mAttack: `Monster hits player with ${monsterAttack} damage`
+            });
+        }
     }
 });
